b97bc253ac8f2eea248900337d4b1c432f970fb7
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2022-05-18 22:44:09 +0600

    daisyui configure

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+        <div className='flex justify-center items-center h-screen'>
+            <div class="card max-w-lg bg-base-100 shadow-xl">
+                <div class="card-body text-center">
+                    <h2 class="text-3xl text-primary font-bold">TO DO APP!</h2>
+                    <div class="card-actions justify-center">
+                    <button class="btn btn-primary">Add Task</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
