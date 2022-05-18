5428fbc7b779dabf274f193f196518d9d950acc4
Md. Abdullah-Al-Asgor Ali <asgor.ice@gmail.com>
2022-05-19 02:57:21 +0600

    Todo App link in list page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,11 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Home = () => {
+    const [user] =useAuthState(auth);
     const navigate = useNavigate();
     const login = () => {
         navigate('/login')
@@ -10,14 +14,27 @@ const Home = () => {
         navigate('/register')
     }
 
+    const handleSignOut = () =>{
+        signOut(auth);
+        navigate('/login');
+    }
+
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className="card max-w-lg bg-base-100 shadow-xl">
                 <div className="card-body text-center">
                     <h2 className="text-3xl text-primary font-bold">TO DO APP!</h2>
                     <div className="card-actions justify-center">
-                    <button className="btn btn-primary" onClick={login}>Login</button>
-                    <button className="btn btn-primary" onClick={register}>Register</button>
+                        {
+                            user?
+                            <button className='btn btn-error' onClick={handleSignOut}>SignOut</button>
+                            :
+                            <>
+                            <button className="btn btn-primary" onClick={login}>Login</button>
+                            <button className="btn btn-primary" onClick={register}>Register</button>
+                            </>
+                        }
+                        
                     </div>
                 </div>
             </div>
--- src/Pages/Home/ToDoList.js
+++ src/Pages/Home/ToDoList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import CustomToDo from '../../Hooks/CustomToDo';
 import ToDoData from './ToDoData';
 
 const ToDoList = () => {
     const [todo, setTodo] = CustomToDo([]);
-    console.log(todo)
+    const navigate = useNavigate();
+    // console.log(todo)
 
     const todoDelete = id =>{
         // console.log(id);
@@ -21,15 +23,20 @@ const ToDoList = () => {
                 setTodo(remaining);
             });
         }
-        console.log(id)
+        // console.log(id)
+    }
+
+    const AddTask = () =>{
+        navigate('/todo');
     }
 
     return (
 
-        <div className='flex justify-center items-center h-screen'>
+        <div className='flex flex-col justify-center items-center h-screen'>
+            <button onClick={AddTask} className='btn btn-primary'>Add a Task</button>
             <div className="card max-w-xl bg-base-100 shadow-xl">
             <div className="card-body">
-            <div className="overflow-x-auto flex justify-center items-center">
+            <div className="overflow-x-auto flex justify-center items-center">  
                 <table className="table table-zebra w-full">
                     <thead>
                     <tr>
